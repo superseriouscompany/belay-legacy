a0a7e648862b1d88b51c2354a334ac158ad87888
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-12-03 21:50:35 +0000

    Attempt to set the icon, but don't actually set the icon

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600, icon: __dirname + '/img/icon.icns'})
   mainWindow.maximize()
 
   // and load the index.html of the app.
