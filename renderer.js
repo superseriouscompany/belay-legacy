86e9513725efd9f32bd8072a52799909695670ec
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-12-04 03:40:21 +0000

    Remove jquery and do animations in css

--- renderer.js
+++ renderer.js
@@ -188,25 +188,18 @@ const storer = {
 
 const tasker = {
   explode: function(cb) {
-    const $o = $(now);
-
-    $o.html($o.text().replace(/([\S])/g, "<span>$1</span>"));
-    $o.css("position", "relative");
-    $("span", $o).each(function(i) {
+    now.innerHTML = now.innerText.replace(/([\S])/g, "<span>$1</span>")
+    now.querySelectorAll('span').forEach(function(s, i) {
+      window.getComputedStyle(s).opacity; // https://timtaubert.de/blog/2012/09/css-transitions-for-dynamically-created-dom-elements/
       var newTop = Math.floor(Math.random()*500)*((i%2)?1:-1);
       var newLeft = Math.floor(Math.random()*500)*((i%2)?1:-1);
+      s.style.position = 'relative';
+      s.style.opacity = 0;
+      s.style.top = `${newTop}px`;
+      s.style.left = `${newLeft}px`;
+    })
 
-      $(this).css({position: "relative",
-        opacity: 1,
-        top: 0,
-        left: 0
-      }).animate({
-        opacity: 0,
-        fontSize: 84,
-        top: newTop,
-        left:newLeft
-      },1000, cb);
-    });
+    setTimeout(cb, 1000);
   }
 }
 
